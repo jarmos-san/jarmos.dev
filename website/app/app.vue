51c07cd50c2a3b35f43a736927b9f1a2fad2ef96
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2026-09-07 15:35:34 +0530

    chore: centralise the app config

--- website/app/app.vue
+++ website/app/app.vue
@@ -5,26 +5,3 @@
     <AppFooter />
   </main>
 </template>
-
-<script lang="ts" setup>
-useHead({
-  htmlAttrs: {
-    lang: "en",
-  },
-  titleTemplate: (title) => {
-    return title ? `${title} | jarmos.dev` : "jarmos.dev";
-  },
-  link: [
-    {
-      rel: "alternate",
-      type: "application/rss+xml",
-      title: "jarmos.dev RSS Feed",
-      href: "/rss.xml",
-    },
-  ],
-});
-</script>
-
-<style>
-@import "~/assets/styles/main.css";
-</style>
