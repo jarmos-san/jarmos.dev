c1a1609f50c7530d6c0e24a802792248496dafa5
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2026-09-09 14:04:43 +0530

    chore(linting): update `style` rules

--- website/oxlint.config.ts
+++ website/oxlint.config.ts
@@ -18,11 +18,19 @@ export default defineConfig({
     defineProps: "readonly",
     process: "readonly",
   },
+  overrides: [
+    {
+      files: ["**/components/**/*.ts", "**/components/**/*.vue"],
+      rules: { "unicorn/filename-case": ["error", { case: "pascalCase" }] },
+    },
+  ],
   plugins: ["typescript", "unicorn", "oxc"],
   rules: {
+    "no-ternary": "off",
     "no-undefined": "off",
     "one-var": "off",
     "oxc/no-async-await": "off",
     "oxc/no-optional-chaining": "off",
+    "sort-imports": "off",
   },
 });
